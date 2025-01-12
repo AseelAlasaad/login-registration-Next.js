c35b2c0b0b2734bb0f9e09ccbd556555cc726f0e
AseelAlasaad <85874977+AseelAlasaad@users.noreply.github.com>
2025-01-12 23:49:30 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const DB_URL="mongodb+srv://aseel:XfNaBEiBnxnM5AkO@cluster0.z9hew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const API= process.env.NODE_ENV === "production"? "http://xxx:3000/api": "http://localhost:3000/api"
-module.exports={
-    DB_URL,
-    API
-}
-
